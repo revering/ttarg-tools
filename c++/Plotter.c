93b4125975f6d5c5aca89c28bbdb077106535f93
Reese Petersen <pet00831@umn.edu>
2018-07-11 10:43:26 -0500

    Plotter.c ranges improved, took out big root file, now on hdfs/cms/user/pet00831/Z_mumu_4vec/

--- c++/Plotter.c
+++ c++/Plotter.c
@@ -10,10 +10,10 @@ void Plotter::book(TDirectory* save_dir, const std::string sub_dir) {
   save_dir->cd(sub_dir.c_str());
   pt_pos = new TH1F("pt_pos","Transverse Momentum (mu+)",100,0,100);
   pt_neg = new TH1F("pt_neg","Transverse Momentum (mu-)",100,0,100);
-  eta_pos = new TH1F("eta_pos","#eta (mu+)",100,-50,50);
-  eta_neg = new TH1F("eta_neg","#eta (mu-)",100,-50,50);
-  phi_pos = new TH1F("phi_pos","#phi (mu+)",360,0,360);
-  phi_neg = new TH1F("phi_neg","#phi (mu+)",360,0,360);
+  eta_pos = new TH1F("eta_pos","#eta (mu+)",160,-8,8);
+  eta_neg = new TH1F("eta_neg","#eta (mu-)",160,-8,8);
+  phi_pos = new TH1F("phi_pos","#phi (mu+)",100,-6.28,6.28);
+  phi_neg = new TH1F("phi_neg","#phi (mu+)",100,-6.28,6.28);
   inv_m = new TH1F("inv_m","Invariant Mass",200,0,200);
 }
 
@@ -22,10 +22,10 @@ void Plotter::book(TFile* save_dir, const std::string sub_dir) {
   save_dir->cd(sub_dir.c_str());
   pt_pos = new TH1F("pt_pos","Transverse Momentum (mu+)",100,0,100);
   pt_neg = new TH1F("pt_neg","Transverse Momentum (mu-)",100,0,100);
-  eta_pos = new TH1F("eta_pos","#eta (mu+)",100,-50,50);
-  eta_neg = new TH1F("eta_neg","#eta (mu-)",100,-50,50);
-  phi_pos = new TH1F("phi_pos","#phi (mu+)",360,0,360);
-  phi_neg = new TH1F("phi_neg","#phi (mu+)",360,0,360);
+  eta_pos = new TH1F("eta_pos","#eta (mu+)",160,-8,8);
+  eta_neg = new TH1F("eta_neg","#eta (mu-)",160,-8,8);
+  phi_pos = new TH1F("phi_pos","#phi (mu+)",100,-6.28,6.28);
+  phi_neg = new TH1F("phi_neg","#phi (mu+)",100,-6.28,6.28);
   inv_m = new TH1F("inv_m","Invariant Mass",200,0,200);
 }
 
@@ -36,8 +36,8 @@ void Plotter::fill(event e) {
   eta_neg->Fill(e.mu_neg_vec.Eta());
   phi_pos->Fill(e.mu_pos_vec.Phi());
   phi_neg->Fill(e.mu_neg_vec.Phi());
-  inv_m->Fill(e.mu_pos_vec.M());
-  inv_m->Fill(e.mu_neg_vec.M());
+  TLorentzVector sum = e.mu_pos_vec+e.mu_neg_vec;
+  inv_m->Fill(sum.M());
 }
 
 void Plotter::write(TDirectory* save_dir) {
